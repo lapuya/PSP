372cf9c371c5b2428e17f2283202113d486d59ea
laurenceapuya <laurence.apuya.p@gmail.com>
2021-10-26 21:12:31 +0200

    Excepciones en Cliente

--- PSP_A2/Cliente.java
+++ PSP_A2/Cliente.java
@@ -4,124 +4,110 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class cliente {
-	
+public class Cliente {
+
 	public static final int PUERTO = 2017;
 	public static final String IP_SERVER = "localhost";
-	
-	
-	
-	
+
 	public static void main(String[] args) throws IOException {
-		
-		
+
 		System.out.println("----------------------------------");
 		System.out.println("--------BIBLIOTECA VIRTUAL--------");
 		System.out.println("----------------------------------");
-		
 
 		InetSocketAddress direccionServidor = new InetSocketAddress(IP_SERVER, PUERTO);
-		
-		
-		try(Scanner sc = new Scanner(System.in);
-				Socket socketServidor = new Socket();){
 
-			boolean continuar = true;			
-			
-			//Bucle do-while para poder hacer tantas peticiones de libros como se quiera hasta que se introduzca un 3.
-			
+		try (Scanner sc = new Scanner(System.in); Socket socketServidor = new Socket();) {
+
+			boolean continuar = true;
+
+			// Bucle do-while para poder hacer tantas peticiones de libros como se quiera
+			// hasta que se introduzca un 3.
+
 			do {
 				System.out.println("");
-				System.out.println("Escriba el número de la acción deseada:");
+				System.out.println("Escriba el nÃºmero de la acciÃ³n deseada:");
 				System.out.println("");
 				System.out.println("1-Consultar libro por ISBN.");
-				System.out.println("2-Consultar libro por título.");
-				System.out.println("3-Salir de la aplicación;");
-				
-			//Se pide número por pantalla para utilizar el menú
+				System.out.println("2-Consultar libro por tÃ­tulo.");
+				System.out.println("3-Salir de la aplicaciÃ³n;");
+
+				// Se pide nÃºmero por pantalla para utilizar el menÃº
 				String n = sc.nextLine();
-				
-				
+
 				switch (n) {
-				
-			//Busqueda de libro por ISBN.
+
+				// Busqueda de libro por ISBN.
 				case "1":
-					
+
 					System.out.println("Introduzca el ISBN del libro deseado: ");
-					
-			//Le añado el 'tipo' de dato que le estoy mandando (ISBN o título) separado por un '-'.
+
+					// Le aÃ±ado el 'tipo' de dato que le estoy mandando (ISBN o tÃ­tulo) separado por
+					// un '-'.
 					String busqueda = sc.nextLine() + "-1";
-					
-			//Conectando el Socket con el servidor.
-					System.out.println("CLIENTE: Esperando a que el servidor acepte la conexión");
+
+					// Conectando el Socket con el servidor.
+					System.out.println("CLIENTE: Esperando a que el servidor acepte la conexiÃ³n");
 					socketServidor.connect(direccionServidor);
-					System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER 
-							+ " por el puerto " + PUERTO);
-					
-			//Lo que envía el Socket.
+					System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER + " por el puerto " + PUERTO);
+
+					// Lo que envÃ­a el Socket.
 					PrintStream salida = new PrintStream(socketServidor.getOutputStream());
 					salida.println(busqueda);
 
-			//Lo que trae de vuelta el Socket.	
-					System.out.println("CLIENTE: Esperando al resultado del servidor...");	
+					// Lo que trae de vuelta el Socket.
+					System.out.println("CLIENTE: Esperando al resultado del servidor...");
 					InputStreamReader entrada = new InputStreamReader(socketServidor.getInputStream());
 					BufferedReader bf = new BufferedReader(entrada);
-					String resultado = bf.readLine();				
-					System.out.println("CLIENTE: El resultado de la búsqueda es: " + resultado.toString());
-					
+					String resultado = bf.readLine();
+					System.out.println("CLIENTE: El resultado de la bÃºsqueda es: " + resultado.toString());
+
 					break;
-					
-								
-					
-			//Repito lo anterior buscando por título.
+
+				// Repito lo anterior buscando por tÃ­tulo.
 				case "2":
-					System.out.println("Introduzca el título del libro deseado: ");
-					
-					//Le añado el 'tipo' de dato que le estoy mandando (ISBN o título)
+					System.out.println("Introduzca el tÃ­tulo del libro deseado: ");
+
+					// Le aÃ±ado el 'tipo' de dato que le estoy mandando (ISBN o tÃ­tulo)
 					busqueda = sc.nextLine() + "-2";
-					
-					System.out.println("CLIENTE: Esperando a que el servidor acepte la conexión");
+
+					System.out.println("CLIENTE: Esperando a que el servidor acepte la conexiÃ³n");
 					socketServidor.connect(direccionServidor);
-					System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER 
-							+ " por el puerto " + PUERTO);	
-					
+					System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER + " por el puerto " + PUERTO);
+
 					salida = new PrintStream(socketServidor.getOutputStream());
 					salida.println(busqueda);
-					
-					
-					System.out.println("CLIENTE: Esperando al resultado del servidor...");	
+
+					System.out.println("CLIENTE: Esperando al resultado del servidor...");
 					entrada = new InputStreamReader(socketServidor.getInputStream());
 					bf = new BufferedReader(entrada);
-					resultado = bf.readLine();				
-					System.out.println("CLIENTE: El resultado de la búsqueda es: " + resultado.toString());
-					
-				
+					resultado = bf.readLine();
+					System.out.println("CLIENTE: El resultado de la bÃºsqueda es: " + resultado.toString());
+
 					break;
-					
-									
-					
-					
-					
+
 				case "3":
 					continuar = false;
 					break;
-					
+
 				default:
 					break;
 				}
-				
-			
-			
-				
-			}while (continuar);
+
+			} while (continuar);
+		} catch (UnknownHostException e) {
+			System.err.println("CLIENTE: No encuentro el servidor en la direcciÃ³n" + IP_SERVER);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("CLIENTE: Error de entrada/salida");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("CLIENTE: Error -> " + e);
+			e.printStackTrace();
 		}
-		
-	
-		
-		
-		
-		
-	}	
+		System.out.println("CLIENTE: Fin del programa");
+	}
 }
