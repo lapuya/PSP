b5cdbd730b7fe2b32b304fc4287581fc7611a4ba
Iván Pérez <79358705+ivaperma@users.noreply.github.com>
2021-11-21 22:46:55 +0100

    Comentado

--- PSP_A3/PSP_A3_REST_Cliente/src/main/java/es/cliente/servicio/ServicioProxyVideojuego.java
+++ PSP_A3/PSP_A3_REST_Cliente/src/main/java/es/cliente/servicio/ServicioProxyVideojuego.java
@@ -12,16 +12,28 @@ import org.springframework.web.client.RestTemplate;
 
 import es.cliente.entidad.Videojuego;
 
+// Damos de alta un objeto tipo ServicioProxyVideojuego en contexto Spring
+
 @Service
 public class ServicioProxyVideojuego {
-	
+
+	// URL del servicio REST de videojuegos
 	public static final String URL = "http://localhost:8080/videojuegos/";
-	
+
+	// Inyectamos un objeto tipo RestTemplate para ayudarnos
+	// a hacer peticiones HTTP al servicio REST
 	@Autowired
 	private RestTemplate restTemplate;
 
-	
-	public Videojuego alta(Videojuego v){
+	/**
+	 * Método para dar de alta un videojuego en el servicio REST
+	 * 
+	 * @param v videojuego que damos de alta
+	 * @return videojuego dado de alta en servicio REST. Null si no se ha podido dar
+	 *         de alta
+	 */
+
+	public Videojuego alta(Videojuego v) {
 		try {
 
 			ResponseEntity<Videojuego> re = restTemplate.postForEntity(URL, v, Videojuego.class);
@@ -29,64 +41,91 @@ public class ServicioProxyVideojuego {
 			return re.getBody();
 		} catch (HttpClientErrorException e) {
 			System.out.println("alta -> El videojuego NO se ha dado de alta, id: " + v);
-		    System.out.println("alta -> Codigo de respuesta: " + e.getStatusCode());
-		    return null;
+			System.out.println("alta -> Codigo de respuesta: " + e.getStatusCode());
+			return null;
 		}
 	}
-	
-	public boolean borrar(int id){
+
+	/**
+	 * Método que borra un videojuego en el servicio REST
+	 * 
+	 * @param id videojuego que queremos borrar
+	 * @return true si lo hemos podido borrar. False en caso contrario.
+	 */
+
+	public boolean borrar(int id) {
 		try {
 
 			restTemplate.delete(URL + id);
 			return true;
 		} catch (HttpClientErrorException e) {
 			System.out.println("borrar -> El videojuego NO se ha podido borrar, id: " + id);
-		    System.out.println("borrar -> Codigo de respuesta: " + e.getStatusCode());
-		    return false;
+			System.out.println("borrar -> Codigo de respuesta: " + e.getStatusCode());
+			return false;
 		}
 	}
-	
-	public boolean modificar(Videojuego v){
+
+	/**
+	 * Método que modifica un videojuego en el servicio REST
+	 * 
+	 * @param v videojuego que vamos a modificar, se hará por ID
+	 * @return true si lo hemos podido modificar. False en caso contrario
+	 */
+
+	public boolean modificar(Videojuego v) {
 		try {
 
 			restTemplate.put(URL + v.getId(), v, Videojuego.class);
 			return true;
 		} catch (HttpClientErrorException e) {
 			System.out.println("modificar -> La persona NO se ha modificado, id: " + v.getId());
-		    System.out.println("modificar -> Codigo de respuesta: " + e.getStatusCode());
-		    return false;
+			System.out.println("modificar -> Codigo de respuesta: " + e.getStatusCode());
+			return false;
 		}
 	}
-	
-	public Videojuego obtener(int id){
+
+	/**
+	 * Método para obtener un videojuego del servicio REST por ID lanza una
+	 * excepción si no existe
+	 * 
+	 * @param id que queremos obtener
+	 * @return videojuego que buscamos, null si no se encuentra en el server o
+	 *         cualquier otro error
+	 */
+
+	public Videojuego obtener(int id) {
 		try {
 			ResponseEntity<Videojuego> re = restTemplate.getForEntity(URL + id, Videojuego.class);
-			HttpStatus hs= re.getStatusCode();
-			if(hs == HttpStatus.OK) {	
+			HttpStatus hs = re.getStatusCode();
+			if (hs == HttpStatus.OK) {
 				return re.getBody();
-			}else {
+			} else {
 				System.out.println("No admitida");
 				return null;
 			}
-		}catch (HttpClientErrorException e) {
+		} catch (HttpClientErrorException e) {
 			System.out.println("obtener -> El videojuego NO se ha encontrado, id: " + id);
-		    System.out.println("obtener -> Codigo de respuesta: " + e.getStatusCode());
-		    return null;
+			System.out.println("obtener -> Codigo de respuesta: " + e.getStatusCode());
+			return null;
 		}
 	}
-	
-	public List<Videojuego> listar(){
 
-		
+	/**
+	 * Método que devuelve una lista de todos los videojuegos
+	 * 
+	 * @return listado de videojuegos
+	 */
+
+	public List<Videojuego> listar() {
+
 		try {
-			ResponseEntity<Videojuego[]> response =
-					  restTemplate.getForEntity(URL ,Videojuego[].class);
+			ResponseEntity<Videojuego[]> response = restTemplate.getForEntity(URL, Videojuego[].class);
 			Videojuego[] arrayPersonas = response.getBody();
-			return Arrays.asList(arrayPersonas);//convertimos el array en un arraylist
+			return Arrays.asList(arrayPersonas);// convertimos el array en un arraylist
 		} catch (HttpClientErrorException e) {
 			System.out.println("listar -> Error al obtener la lista de videojuegos");
-		    System.out.println("listar -> Codigo de respuesta: " + e.getStatusCode());
-		    return null;
+			System.out.println("listar -> Codigo de respuesta: " + e.getStatusCode());
+			return null;
 		}
 	}
 
